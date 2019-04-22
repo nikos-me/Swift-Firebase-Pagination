08f698390fbd08ef1f84414b05c073bae2da633b
Nikos Menexis <men.nikos@gmail.com>
2019-04-22 21:56:32 +0300

    initial-commit

---
+++ Firebase-Pagination/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Pagination
+//
+//  Created by Nikos Menexis on 22/04/2019.
+//  Copyright © 2019 Nikos Menexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Firebase-PaginationTests/Firebase_PaginationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Firebase_PaginationTests.swift
+//  Firebase-PaginationTests
+//
+//  Created by Nikos Menexis on 22/04/2019.
+//  Copyright © 2019 Nikos Menexis. All rights reserved.
+//
+
+import XCTest
+@testable import Firebase_Pagination
+
+class Firebase_PaginationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
